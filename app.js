79813015e18927de56b81f5582ca6b42e92a7c47
Kenneth Sng <kenneth@open.gov.sg>
2023-07-13 23:43:44 +0800

    Using res.render to insert a specific value into the page

--- app.js
+++ app.js
@@ -3,11 +3,43 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Hello!");
+  var today = new Date();
+  var currentDay = today.getDay();
+  console.log(currentDay);
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: You printed a day that is " + currentDay);
+      break;
+  }
+
+  res.render("list", { dayOfWeek: day });
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server is running on Port 3000.");
